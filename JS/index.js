36c45c037b51a35cf92cc463a7b8ceeff2c43904
yosuvacupice <147127503+yosuvacupice@users.noreply.github.com>
2025-08-30 01:07:01 +0530

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links");
+// const closeBtn = document.querySelector(".close-btn");
+
+// hamburger.addEventListener("click", () => {
+//   navLinks.classList.add("show");
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   navLinks.classList.remove("show");
+// });
